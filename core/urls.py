8419ff95de548ad191adcc406e782427620c9a49
jaeech <58973970+jaeech@users.noreply.github.com>
2020-07-28 21:25:33 +0200

    #11.7 Class Based View 1st
    #11.7 Class Based View 1st

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from rooms import views as room_views
 # app_name은  config.urls.py 내에 있는 namespace 명칭과 동일해야함
 app_name = "core"
 
-urlpatterns = [path("", room_views.all_rooms, name="home")]
+# views에서 생성한 Class를 불러오면서
+# .as_view()를 붙여줌으로서 작동하게만듦(function만 불러올 수 있어서)
+urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]
 
--- rooms/views.py
+++ rooms/views.py
@@ -1,19 +1,15 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    # page key가 없을 경우에 page=1 을 자동적용
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
+class HomeView(ListView):
+    """ HomeView Definition"""
 
-    try:
-        # page는 get_page와 다르게 Error Handling이 수동으로 이루어져야 함
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
+    model = models.Room
+    paginate_by = 10
+    parginate_orphans = 5
+    ordering = "created"
+    # page를 위한 키워드도 지정 가능
+    # 아니면 자동으로 됨
+    # page_kwarg = "anykeyword"
 
-    except Exception:
-        # 기본페이지로 이동
-        return redirect("/")
