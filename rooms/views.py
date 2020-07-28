08e47b6de12ec486df449272aa4eb4be6ccf627a
jaeech <58973970+jaeech@users.noreply.github.com>
2020-07-28 21:38:40 +0200

    #11.8 Class Based Views part 2
    Class Based Views with get_context_data

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.views.generic import ListView
 from . import models
 
@@ -9,6 +10,15 @@ class HomeView(ListView):
     paginate_by = 10
     parginate_orphans = 5
     ordering = "created"
+    # object_list의 명칭을 직정해줄 수 있음
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+
     # page를 위한 키워드도 지정 가능
     # 아니면 자동으로 됨
     # page_kwarg = "anykeyword"
