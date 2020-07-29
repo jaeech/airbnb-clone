4fcf708f26f3ea9fbf30afb04a52baeeb8cd9a19
jaeech <58973970+jaeech@users.noreply.github.com>
2020-07-29 18:52:00 +0200

    # 12.4 Detail View in CBV
    Deatil view made with class based view

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,4 @@
-from django.views.generic import ListView
-from django.http import Http404
-
-# from django.urls import reverse
-# redirect 추가하여 사용도 가능
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -21,13 +16,10 @@ class HomeView(ListView):
     # page_kwarg = "anykeyword"
 
 
-# pk의 경우, urls.py에 입력해놓은 path("<int:pk>" 의 명칭과 동일하게 가야함
-# pk는 URL에 입력된 rooms 뒤에 입력된 url 내 숫자를 받아오게 됨
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
-        # Error 발생 시 home으로 redirect도 가능
-        # return redirect(reverse("core:home"))
+class RoomDetail(DetailView):
+    """ RoomDetail Definition"""
+
+    model = models.Room
+    # pk를 바꿀 수도 있음
+    # pk_url_kwarg = "potato"
+
