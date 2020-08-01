d82a7bc244255c96c8b7683d92937f3521d8e058
jaeech <58973970+jaeech@users.noreply.github.com>
2020-08-01 23:53:30 +0200

    #13.4 finished form (fbv)
    #13.4 finished form (fbv)

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,5 +6,8 @@ app_name = "rooms"
 # <int:pk> 를 입력함으로서 url에 입력된 숫자를 pk로 인식해서 가져옴
 # html에 넣을 argument는 <int:pk>의 pk가 넘어가는 것
 # Detailview에서 자동으로 pk를 끌어가서 view를 설정함
-urlpatterns = [path("<int:pk>", views.RoomDetail.as_view(), name="detail")]
+urlpatterns = [
+    path("<int:pk>", views.RoomDetail.as_view(), name="detail"),
+    path("search/", views.search, name="search"),
+]
 
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 
@@ -22,4 +24,50 @@ class RoomDetail(DetailView):
     model = models.Room
     # pk를 바꿀 수도 있음
     # pk_url_kwarg = "potato"
+
+
+def search(request):
+    # city=입력이 없을 경우 Anywhere로 셋팅되게
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    # pk는 int임으로 변경 필요
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    room_type = int(request.GET.get("room_type", 0))
+    # 여러개의 내역을 받아야하는데 리스트로 받아야해서
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+    # **로 dict 안에 있는 내용을 unpack 해줌
+    return render(request, "rooms/search.html", {**form, **choices})
 
