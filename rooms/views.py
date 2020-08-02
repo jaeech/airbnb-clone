f22fffe37f6b0cc42dd9ddc1b9faccc2b7c45c81
jaeech <58973970+jaeech@users.noreply.github.com>
2020-08-02 12:13:04 +0200

    #13.6 Add Filtering to site(fbv)
    #13.6 Add Filtering to site(fbv)

--- rooms/views.py
+++ rooms/views.py
@@ -39,8 +39,8 @@ def search(request):
     baths = int(request.GET.get("baths", 0))
     room_type = int(request.GET.get("room_type", 0))
     # 여러개의 내역을 받아야하는데 리스트로 받아야해서
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
 
@@ -56,7 +56,7 @@ def search(request):
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
     }
 
     room_types = models.RoomType.objects.all()
@@ -68,6 +68,52 @@ def search(request):
         "amenities": amenities,
         "facilities": facilities,
     }
+
+    # filtering을 위한 argument를 모으는 것
+    # argument의 앞에 이름은 model의 이름을 따라야 함
+    # model 항목 내에 들어가고 싶으면 __ 를 붙여줘야함
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type != 0:
+        filter_args["room_type__pk"] = room_type
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests_gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            filter_args["amenities__pk"] = int(s_amenity)
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            filter_args["facilities__pk"] = int(s_facility)
+
+    # filter를 위한 QuerySet/argutments
+    rooms = models.Room.objects.filter(**filter_args)
+
     # **로 dict 안에 있는 내용을 unpack 해줌
-    return render(request, "rooms/search.html", {**form, **choices})
+    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms})
 
