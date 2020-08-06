ef01a0ae8e9b6c45a357d8adc4d1626f97a2b05d
jaeech <58973970+jaeech@users.noreply.github.com>
2020-08-06 10:53:26 +0200

    #14.4 Login and Logout
    #14.4 Login and Logout(fbv)

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
@@ -14,6 +15,16 @@ class LoginView(View):
             # forms.py에서 데이터를 clean 후
             # clean된 data를 돌려줌
             # form.cleaned_data
-            print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
 
         return render(request, "users/login.html", {"form": form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
